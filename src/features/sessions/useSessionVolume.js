94328db00af212a07e460e1ee832fe423e5d924c
Ikumo Takahashi <ikumo1118free@gmail.com>
2024-03-10 12:42:48 +0900

    Added ingore null and setting default value

--- src/features/sessions/useSessionVolume.js
+++ src/features/sessions/useSessionVolume.js
@@ -2,14 +2,26 @@ import { useQuery } from '@tanstack/react-query';
 import { getTrack } from '../../services/apiTracks';
 import { format } from 'date-fns';
 
+// data == nullの時は0をセットする
+function nullSetDefault(value) {
+  if (value === null) return 0;
+  else return value;
+}
+
+// track == nullの時は無視する
+function IgnoreNull(value, arr) {
+  if (value === null) return;
+  else arr.push(value);
+}
+
 function calcVolume(data) {
   // session内にある一つ一つのtrackのvolume(rep * weight)を文、配列にまとめる
   const trackVolume = data.map((track) => {
     return (
-      track.set_1 * track.weight_1 +
-      track.set_2 * track.weight_2 +
-      track.set_3 * track.weight_3 +
-      track.set_4 * track.weight_4
+      nullSetDefault(track.set_1) * nullSetDefault(track.weight_1) +
+      nullSetDefault(track.set_2) * nullSetDefault(track.weight_2) +
+      nullSetDefault(track.set_3) * nullSetDefault(track.weight_3) +
+      nullSetDefault(track.set_4) * nullSetDefault(track.weight_4)
     );
   });
   // 一つのsessionのvolumeを全て合算した値をreturnする
@@ -19,12 +31,13 @@ function calcVolume(data) {
 async function fetchSessionData(sessions) {
   // ある部位にて、一つ一つのsessionごとに分け、one-sessionのvolumeを算出する
   const sessionDataPromises = sessions.map(async (session) => {
-    const idArr = [
-      session.workout_track1Id,
-      session.workout_track2Id,
-      session.workout_track3Id,
-      session.workout_track4Id,
-    ];
+    const idArr = [];
+
+    IgnoreNull(session.workout_track1Id, idArr);
+    IgnoreNull(session.workout_track1Id, idArr);
+    IgnoreNull(session.workout_track3Id, idArr);
+    IgnoreNull(session.workout_track4Id, idArr);
+
     const data = await getTrack(idArr);
     // 一つのsessino-4種類のworkout-(4set * rep)を算出する
     const sessionVolume = calcVolume(data);
